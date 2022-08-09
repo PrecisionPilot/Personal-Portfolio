6bff155e4a430fb108da1fa15d72dc769286395c
PizzaPrix <seanwaterloo2997@gmail.com>
2022-08-08 21:03:31 -0400

    Add navigation

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import {BsLinkedin} from "react-icons/bs"
-import {FaGithub} from "react-icons/fa"
-import {SiDevpost} from "react-icons/si"
+// import {BsLinkedin} from "react-icons/bs"
+// import {FaGithub} from "react-icons/fa"
+// import {SiDevpost} from "react-icons/si"
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/pilotprix/" target="_blank"><BsLinkedin /></a>
+        {/* <a href="https://www.linkedin.com/in/pilotprix/" target="_blank"><BsLinkedin /></a>
         <a href="https://github.com/PilotPrix" target="_blank"><FaGithub /></a>
-        <a href="https://devpost.com/PilotPrix" target="_blank"><SiDevpost /></a>
+        <a href="https://devpost.com/PilotPrix" target="_blank"><SiDevpost /></a> */}
     </div>
   )
 }
